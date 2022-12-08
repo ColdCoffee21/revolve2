72899046a3bcc9c2aec344d411afab84b42e5e82
ColdCoffee21 <dheerajvarghese@gmail.com>
2022-12-09 00:50:03 +0100

    Clearance

--- runners/isaacgym/revolve2/runners/isaacgym/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Physics runner using the Isaac Gym simulator."""
-
-from ._local_runner import LocalRunner
-from ._modular_robot_rerunner import ModularRobotRerunner
-
-__all__ = ["LocalRunner", "ModularRobotRerunner"]
